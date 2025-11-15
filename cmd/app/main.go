ae034b54fbeb0d7a3405e083e7c1139470c4c4da
TimSin5599 <timasinitsyn@bk.ru>
2025-11-15 17:13:15 +0300

    init

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,15 +5,18 @@ import (
 
 	"github.com/evrone/go-clean-template/config"
 	"github.com/evrone/go-clean-template/internal/app"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Configuration
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: No .env file found: %v", err)
+	}
+
 	cfg, err := config.NewConfig()
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
 	}
 
-	// Run
 	app.Run(cfg)
 }
